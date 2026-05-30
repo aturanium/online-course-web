c240393a448711dda0c5ac7702e0255f018e58f3
aturanium <tatstar2k5@gmail.com>
2026-05-30 18:16:48 +0700

    fix

--- src/configs/Firebase.js
+++ src/configs/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCSHx5HVbigaiWUj9cDh0WF-ts0tAa_3E",
-  authDomain: "onlinecourse-728de.firebaseapp.com",
-  databaseURL: "https://onlinecourse-728de-default-rtdb.firebaseio.com/",
-  projectId: "onlinecourse-728de",
-  storageBucket: "onlinecourse-728de.firebasestorage.app",
-  messagingSenderId: "680842250341",
-  appId: "1:680842250341:web:2b88e5964e04bf53e4de8c",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
